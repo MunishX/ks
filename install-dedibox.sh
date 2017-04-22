3b82c25ec24d05db1f07cb8baf2956e8be009e62
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-23 01:31:21 +0530

    Update install-dedibox.sh

--- install-dedibox.sh
+++ install-dedibox.sh
@@ -22,12 +22,12 @@ curl -o /boot/vmlinuz ${MIRROR}images/pxeboot/vmlinuz
 curl -o /boot/initrd.img ${MIRROR}images/pxeboot/initrd.img
 
 #    linux /vmlinuz net.ifnames=0 biosdevname=0 ip=${IPADDR}::${GW}:${PREFIX}:$(hostname):eth0:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=$KSURL
-# vncconnect=${IPADDR}:5500 # inst.vnc inst.vncpassword=changeme 
+# inst.vncconnect=${IPADDR}:5500 # inst.vnc inst.vncpassword=changeme headless
 
 cat << EOF >> /etc/grub.d/40_custom
 menuentry "reinstall" {
     set root=(hd0,1)
-    linux /vmlinuz inst.vnc inst.vncconnect=${IPADDR}:5500 inst.vncpassword=changeme headless net.ifnames=0 biosdevname=0 ip=${IPADDR}::${GW}:${PREFIX}:$(hostname):eth0:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=$KSURL
+    linux /vmlinuz inst.vnc inst.vncpassword=changeme net.ifnames=0 biosdevname=0 ip=${IPADDR}::${GW}:${PREFIX}:$(hostname):eth0:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=$KSURL
     initrd /initrd.img
 }
 EOF
