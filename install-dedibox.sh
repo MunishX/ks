dbfc8fa5b6b6efd3334fe3f25651552543595309
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-23 00:32:37 +0530

    Update install-dedibox.sh

--- install-dedibox.sh
+++ install-dedibox.sh
@@ -5,13 +5,15 @@
 
 # see README.md
 
-export INSTALL_SRV="http://KICKSTART_SRV_FQDN/"
+#export INSTALL_SRV="http://KICKSTART_SRV_FQDN/"
 
-export KSURL="${INSTALL_SRV}dedibox.cfg"
+export KSURL="https://github.com/munishgaurav5/ks/raw/master/dedibox.cfg"
 export DNS1=8.8.8.8
 export DNS2=8.8.4.4
 
-export MIRROR="http://mirror.ircam.fr/pub/CentOS/7.2.1511/os/x86_64/"
+#export MIRROR="http://mirror.ircam.fr/pub/CentOS/7.2.1511/os/x86_64/"
+export MIRROR="http://mirror.nl.leaseweb.net/centos/7/os/x86_64/"
+
 export IPADDR=$(ip a s eth0 |grep "inet "|awk '{print $2}'| awk -F '/' '{print $1}')
 export PREFIX=$(ip a s eth0 |grep "inet "|awk '{print $2}'| awk -F '/' '{print $2}')
 export GW=$(ip route|grep default | awk '{print $3}')
@@ -19,10 +21,12 @@ export GW=$(ip route|grep default | awk '{print $3}')
 curl -o /boot/vmlinuz ${MIRROR}images/pxeboot/vmlinuz
 curl -o /boot/initrd.img ${MIRROR}images/pxeboot/initrd.img
 
+#    linux /vmlinuz net.ifnames=0 biosdevname=0 ip=${IPADDR}::${GW}:${PREFIX}:$(hostname):eth0:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=$KSURL
+
 cat << EOF >> /etc/grub.d/40_custom
 menuentry "reinstall" {
     set root=(hd0,1)
-    linux /vmlinuz net.ifnames=0 biosdevname=0 ip=${IPADDR}::${GW}:${PREFIX}:$(hostname):eth0:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=$KSURL
+    linux /vmlinuz vncconnect=${IPADDR}:5500 vncpassword=changeme headless net.ifnames=0 biosdevname=0 ip=${IPADDR}::${GW}:${PREFIX}:$(hostname):eth0:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=$KSURL
     initrd /initrd.img
 }
 EOF
@@ -31,4 +35,11 @@ sed -i -e "s/GRUB_DEFAULT.*/GRUB_DEFAULT=\"reinstall\"/g" /etc/default/grub
 
 grub2-mkconfig
 grub2-mkconfig --output=/boot/grub2/grub.cfg
+
+echo ""
+echo ""
+echo "IP : $IPADDR"
+echo "Gateway : $GW"
+echo ""
+echo "DONE!"
 
