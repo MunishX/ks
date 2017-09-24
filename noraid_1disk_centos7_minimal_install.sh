d44e5844fef5802945dfa6821ffeff499faba21f
munishgaurav5 <munishgaurav5@gmail.com>
2017-09-24 23:43:48 +0530

    Update noraid_1disk_centos7_minimal_install.sh

--- noraid_1disk_centos7_minimal_install.sh
+++ noraid_1disk_centos7_minimal_install.sh
@@ -14,7 +14,9 @@ export DNS1=8.8.8.8
 export DNS2=8.8.4.4
 
 #export MIRROR="http://mirror.ircam.fr/pub/CentOS/7.2.1511/os/x86_64/"
-export MIRROR="http://mirror.nl.leaseweb.net/centos/7/os/x86_64/"
+#export MIRROR="http://mirror.nl.leaseweb.net/centos/7/os/x86_64/"
+#export MIRROR="http://mirror.centos.org/centos/7/os/x86_64/"
+export MIRROR="http://mirror.imt-systems.com/centos/7/os/x86_64/"  
 
 export IPADDR=$(ip a s $NETWORK_INTERFACE_NAME |grep "inet "|awk '{print $2}'| awk -F '/' '{print $1}')
 export PREFIX=$(ip a s $NETWORK_INTERFACE_NAME |grep "inet "|awk '{print $2}'| awk -F '/' '{print $2}')
