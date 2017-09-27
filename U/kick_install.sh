330b8a5d0ccaa1154957ca4c5d3b0f6d47090c92
munishgaurav5 <munishgaurav5@gmail.com>
2017-09-27 22:28:09 +0530

    Update kick_install.sh

--- U/kick_install.sh
+++ U/kick_install.sh
@@ -10,12 +10,13 @@ apt-get upgrade -y
 apt-get install -y nano wget curl net-tools lsof bzip2 zip unzip git sudo make cmake sed at
 
 # configs
+
+export KSURL="https://github.com/munishgaurav5/ks/raw/master/U/kick_tmp.cfg"
+export MIRROR="http://fastserver.me/admin/iso/"  
+
+
 NETWORK_INTERFACE_NAME="$(ip -o -4 route show to default | awk '{print $5}')"
-
-
-export KSURL="https://github.com/munishgaurav5/ks/raw/master/noraid_1disk_centos7_minimal.cfg"
-export MIRROR="http://mirror.imt-systems.com/centos/7/os/x86_64/"  
-
+Boot_device="eth0"
 
 export DNS1=8.8.8.8
 export DNS2=8.8.4.4
@@ -26,6 +27,15 @@ export GW=$(ip route|grep default | awk '{print $3}')
 
 ##### SETUP
 
-curl -o /boot/vmlinuz ${MIRROR}images/pxeboot/vmlinuz
-curl -o /boot/initrd.img ${MIRROR}images/pxeboot/initrd.img
+curl -o /boot/ubuntu_vmlinuz ${MIRROR}install/vmlinuz
+curl -o /boot/ubuntu_initrd.gz ${MIRROR}install/initrd.gz
+
+echo ""
+echo ""
+root_value=`grep "set root=" /boot/grub2/grub.cfg | head -1`
+echo "$root_value"
+echo ""
+echo ""
+sleep 2
+echo ""
 
