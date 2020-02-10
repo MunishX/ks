f217ed1e8df8168d285b4f5537adab90edcdaed8
munishgaurav5 <munishgaurav5@gmail.com>
2020-02-10 19:09:42 +0530

    Update cloud_install.sh

--- HZ/cloud_install.sh
+++ HZ/cloud_install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-# cd /tmp && wget https://github.com/munishgaurav5/ks/raw/master/HZ/cloud_install.sh && chmod 777 cloud_install.sh && ./cloud_install.sh 
+# cd /tmp && yum install wget -y && wget https://github.com/munishgaurav5/ks/raw/master/HZ/cloud_install.sh && chmod 777 cloud_install.sh && ./cloud_install.sh 
 
 echo ""
 echo ""
