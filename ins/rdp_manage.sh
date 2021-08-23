56052a5f1e0ec19478001a592183f4c51d06697f
munishgaurav5 <munishgaurav5@gmail.com>
2021-08-23 14:01:47 +0530

    Update rdp_manage.sh

--- ins/rdp_manage.sh
+++ ins/rdp_manage.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-
-action=$1
-echo ""
-
+#action=$1 echo ""
 #    while [[ $action = "" ]]; do # to be replaced with regex
-#       read -p "(1/6) Enter Username (user): " User_Name
-#    done
-
-if [ -z "${action}" ];
-then
-    echo "true"
+#       read -p "(1/6) Enter Username (user): " User_Name done
+if [ -z "$1" ]; then
+    echo "info"
 else
-    echo "false"
+    action=$1
+    echo "action :" $1
 fi
 
 echo ""
 dname="Ricardo"
-read -p "Enter your name [${dname}]: " name
-#read -e -i "$name" -p "Enter your name: " input
-name="${name:-$dname}"
+#read -p "Enter your name [${dname}]: " name
+read -e -i "$dname" -p "Enter your name: " name
+
+if [ -z "${name}" ]; then
+    name=${dname}
+else
+    echo ""
+fi
 
 echo ""
-echo $name 
+echo $name
 echo ""
-
