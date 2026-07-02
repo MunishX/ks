92427f6336d9adbd706e912fa42d11aaafc81aa7
MunishX <munishgaurav5@gmail.com>
2026-07-02 12:10:52 +0530

    Update aruba_hdd_net_install_rocky9.sh

--- HZ/aruba_hdd_net_install_rocky9.sh
+++ HZ/aruba_hdd_net_install_rocky9.sh
@@ -206,6 +206,8 @@ fi
 # you can use grub2-set-default 'MenuEntry' 
 # to change the default boot
 
+echo ""
+echo ""
 echo "CURRENT DNS"
 ( nmcli dev list || nmcli dev show ) 2>/dev/null | grep DNS
 
