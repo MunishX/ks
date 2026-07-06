2a9cd0adc0431948c42b1ab12adc5cbc7412d62e
MunishX <munishgaurav5@gmail.com>
2026-07-06 14:50:27 +0530

    Update ks.sh

--- new/ks.sh
+++ new/ks.sh
@@ -275,7 +275,8 @@ prepare_uuid(){
     if [ $EXIT_CODE -eq 0 ]
     then
         IS_RAID=1
-        ROOT_MDUUID_DEV=`mdadm --detail ${ROOT_DEV} | grep -E '/dev/' |  awk '{print $2}'`
+        #ROOT_MDUUID_DEV=`mdadm --detail ${ROOT_DEV} | grep -E '/dev/' |  awk '{print $2}'`
+        ROOT_MDUUID_DEV=`mdadm --detail ${ROOT_DEV} | grep -E '/dev/' | awk 'NR==2 {print $7}'`
         ROOT_MDUUID=`lsblk -lp -o "Name,UUID,MOUNTPOINT" | grep "${ROOT_MDUUID_DEV}$" | head -1 | awk  '{print $2}'`
         ROOT_MDUUID_CLEAN="${ROOT_MDUUID//-/}"
         ROOT_UUID_LINE="set root='mduuid/${ROOT_MDUUID_CLEAN}'";
