90bf5dea0e0341c994e11780813c1b16c0673bef
MunishX <munishgaurav5@gmail.com>
2026-07-06 21:24:11 +0530

    Update ks.sh

--- new/ks.sh
+++ new/ks.sh
@@ -41,7 +41,7 @@ init(){
 read_hostname(){
     my_host=$(hostname)
     #read -p "Enter new server's Hostname: (ex: host.server.com) : " HOSTNAME
-    read -e -i "${my_host}" -p "Enter new server's Hostname (in format host.server.com): " HOSTNAME
+    read -e -i "${my_host}" -p "Enter new Hostname (format host.server.com) : " HOSTNAME
     
     if [[ -z "$HOSTNAME" ]]; then
             echo "[error] HOSTNAME cannot be empty, Failed.. Exiting..."
@@ -269,7 +269,7 @@ prepare_uuid(){
 
     ROOT_UUID_LINE="search --no-floppy --fs-uuid --set=root ${ROOT_UUID}"
 
-    
+    # RAID EXTRA SUPPORT (not required)
     #IS_RAID=0
     #mdadm --detail ${ROOT_DEV} > /dev/null 2>&1
     #EXIT_CODE=$?
@@ -391,8 +391,8 @@ menuentry "reinstall" {
     #insmod raid6rec
     
     $ROOT_UUID_LINE
-    #${LINUX_VAR} ${BOOT_PATH}vmlinuz ip=${IPADDR}::${GW}:${PREFIX}:${HOSTNAME}:${NETWORK_INTERFACE_NAME}:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=hd:UUID=${ROOT_UUID}:${BOOT_PATH}${KSFName} inst.lang=en_US inst.keymap=us inst.vnc ${CONFIG_APPEND_LINE}
-    ${LINUX_VAR} ${BOOT_PATH}vmlinuz ip=${IPADDR}::${GW}:${NETMASK}:${HOSTNAME1}:${NETWORK_INTERFACE_NAME}:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=hd:${ROOT_DEV}:${BOOT_PATH}${KSFName} inst.lang=en_US inst.keymap=us inst.vnc ${CONFIG_APPEND_LINE}
+    ${LINUX_VAR} ${BOOT_PATH}vmlinuz ip=${IPADDR}::${GW}:${PREFIX}:${HOSTNAME}:${NETWORK_INTERFACE_NAME}:none nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=hd:UUID=${ROOT_UUID}:${BOOT_PATH}${KSFName} inst.lang=en_US inst.keymap=us inst.vnc ${CONFIG_APPEND_LINE}
+    #${LINUX_VAR} ${BOOT_PATH}vmlinuz ip=${IPADDR}::${GW}:${NETMASK}:${HOSTNAME1}:${NETWORK_INTERFACE_NAME}:off nameserver=$DNS1 nameserver=$DNS2 inst.repo=$MIRROR inst.ks=hd:${ROOT_DEV}:${BOOT_PATH}${KSFName} inst.lang=en_US inst.keymap=us inst.vnc ${CONFIG_APPEND_LINE}
     ${INITRD_VAR} ${BOOT_PATH}initrd.img
 }
 # reinstall_menu_entry_end
